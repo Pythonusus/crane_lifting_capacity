b8495fb6af4a5d42745a7c5884054a1f58a1cb7f
Petr Malafeev <StandBy545@yandex.ru>
2025-07-10 15:50:18 +0300

    Remove(fe): redundant font import in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import 'semantic-ui-css/semantic.min.css'
-import './styles/fonts.css'
 import './index.css'
 import App from './App.jsx'
 
