32e9084d2da6e9d3b3cfdae56d746552e9e4f4f8
Petr Malafeev <StandBy545@yandex.ru>
2025-07-27 12:18:57 +0300

    Test(be): utils/math module

---
+++ backend/tests/test_utils/test_math.py
@@ -0,0 +1,41 @@
+import pytest
+
+from utils.math import (
+    get_nearest_greater,
+    get_nearest_lesser,
+    interpolate_1d,
+)
+
+
+def test_get_nearest_lesser(cranes_test_data):
+    # Get all radiuses for boom length 36.0 and convert them to floats
+    num_list = [
+        float(radius)
+        for radius in cranes_test_data["Takraf РДК250"]["lc_table"][
+            "32.5"
+        ].keys()
+    ]
+    assert get_nearest_lesser(16, num_list) == 15
+    assert get_nearest_lesser(15.5, num_list) == 15
+    assert get_nearest_lesser(1, num_list) is None
+    assert get_nearest_lesser(5, num_list) is None
+
+
+def test_get_nearest_greater(cranes_test_data):
+    # Get all radiuses for boom length 36.0 and convert them to floats
+    num_list = [
+        float(radius)
+        for radius in cranes_test_data["Liebherr LR1100"]["lc_table"][
+            "50.0"
+        ].keys()
+    ]
+    assert get_nearest_greater(10.32, num_list) == 12
+    assert get_nearest_greater(16, num_list) == 18
+    assert get_nearest_greater(1000, num_list) is None
+    assert get_nearest_greater(50, num_list) is None
+
+
+def test_interpolate_1d():
+    assert interpolate_1d(12.5, 12, 56.4, 13, 55.5) == 55.95
+    assert interpolate_1d(12.5, 12, 56.4, 13, 56.4) == 56.4
+    assert pytest.raises(ValueError, interpolate_1d, 12.5, 12, 56.4, 12, 58.0)
