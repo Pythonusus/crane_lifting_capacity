e813005413f509a33ade1c4dc4ebf954dc3bfc22
Petr Malafeev <StandBy545@yandex.ru>
2025-08-05 14:56:47 +0300

    Upd(fe): navbar

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -7,19 +7,19 @@ const Navbar = () => (
   <Menu as='nav' className='custom-navbar' pointing secondary>
     <Menu.Item as={NavLink} exact to='/'>
       <Icon name='cog' size='large' />
-      <span className='hide-on-tablet'>Выбрать кран</span>
+      <span className='font-size-4 hide-on-tablet'>Выбрать кран</span>
     </Menu.Item>
     <Menu.Item as={NavLink} to='/compare'>
       <Icon name='table' size='large' />
-      <span className='hide-on-tablet'>Таблица сравнения</span>
+      <span className='font-size-4 hide-on-tablet'>Таблица сравнения</span>
     </Menu.Item>
     <Menu.Item as={NavLink} to='/history'>
       <Icon name='history' size='large' />
-      <span className='hide-on-tablet'>История расчетов</span>
+      <span className='font-size-4 hide-on-tablet'>История расчетов</span>
     </Menu.Item>
     <Menu.Item as={NavLink} to='/about'>
       <Icon name='info circle' size='large' />
-      <span className='hide-on-tablet'>О программе</span>
+      <span className='font-size-4 hide-on-tablet'>О программе</span>
     </Menu.Item>
   </Menu>
 )
