042ca67192264411f7f57cd2f66e685eeaa93fb3
Petr Malafeev <StandBy545@yandex.ru>
2025-09-25 17:54:48 +0300

    Upd(fe): hide copyright section

--- frontend/src/components/Copyright/index.jsx
+++ frontend/src/components/Copyright/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Copyright.css'
-import { Icon } from 'semantic-ui-react'
+// import { Icon } from 'semantic-ui-react'
 
 const Copyright = () => {
   return (
     <div className='copyright center-content'>
       <div className='github-icon-container'>
-        <Icon name='github' size='big' />
+        {/* <Icon name='github' size='big' /> */}
       </div>
-      Разработал Pythonusus
+      {/* Разработал Малафеев Петр */}
     </div>
   )
 }
